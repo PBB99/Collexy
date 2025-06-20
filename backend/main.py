2ac76017dc98b96a0a34904fb0ffc0cf8bf22d4a
PBB99 <pablbobb9924@gmail.com>
2025-06-20 23:56:40 +0200

    My_products CRUD

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
 
 from models.product import new_product
+from datetime import datetime
 
-test_product=new_product('test','1',15,'new',1,'1',24.5,30.2,'test.com','esto es una prueba')
+test_product=new_product('test','1',15,'NM',True,'1',24.5,30.2,'test.com','esto es una prueba')
 print(test_product)
 
 
--- backend/models/product.py
+++ backend/models/product.py
@@ -1,18 +1,34 @@
 from db.db import get_connection
 from psycopg2 import OperationalError, IntegrityError, ProgrammingError
+import traceback
+from datetime import datetime
 def new_product(NAME,PRODUCT_TYPE_ID,AMOUNT,STATUS,GRADED,GRADING_COMPANY_ID,PRICE,LAST_SOLD_PRICE,URL,DESCRIPTION):
     try:
         conn=get_connection()
         cursor=conn.cursor()
-        cursor.execute("SELECT NEXT_ID FROM UNIQUE_KEYS WHERE TABLE_NAME='MY_PRODUCTS';")
-        id=cursor.fetchone()[0]
+        cursor.execute("SELECT * FROM UNIQUE_KEYS WHERE TABLE_NAME='MY_PRODUCTS';")
+        result=cursor.fetchone()
+        id=result["next_id"]
+        
         query=""" 
             INSERT INTO MY_PRODUCTS (ID,NAME,PRODUCT_TYPE_ID,AMOUNT,STATUS,GRADED,GRADING_COMPANY_ID,PRICE,LAST_SOLD_PRICE,URL,DESCRIPTION)
             VALUES (%s, %s, %s, %s,%s, %s, %s, %s, %s, %s,%s);
         """
-        cursor.execute(query, (id,NAME,PRODUCT_TYPE_ID,AMOUNT,STATUS,GRADED,GRADING_COMPANY_ID,PRICE,LAST_SOLD_PRICE,URL,DESCRIPTION))
-        update_uk="UPDATE UNIQUE_KEYS SET NEXT_ID=(SELECT NEXT_ID+1 FROM UNIQUE_KEYS WHERE TABLE_NAME='MY_PRODUCTS') where TABLE_NAME='MY_PRODUCTS';"
-        cursor.execute(update_uk,id)
+        insert=cursor.execute(query, (id,NAME,PRODUCT_TYPE_ID,AMOUNT,STATUS,GRADED,GRADING_COMPANY_ID,PRICE,LAST_SOLD_PRICE,URL,DESCRIPTION))
+        if  cursor.rowcount >0:
+            cursor.execute("SELECT * FROM UNIQUE_KEYS WHERE TABLE_NAME='HISTORY_PRICES';")
+            result=cursor.fetchone()
+            h_id=result["next_id"]
+            query2="""
+            INSERT INTO HISTORY_PRICES values(%s,%s,%s,%s,%s,%s)
+            """
+            cursor.execute(query2,(h_id,NAME,PRICE,datetime.now(),'',1))
+            if cursor.rowcount>0:
+                update_uk_hprice="UPDATE UNIQUE_KEYS SET NEXT_ID=NEXT_ID+1 where TABLE_NAME='HISTORY_PRICES';"
+                cursor.execute(update_uk_hprice,h_id)
+                if insert is not None:
+                        update_uk_products="UPDATE UNIQUE_KEYS SET NEXT_ID=NEXT_ID+1 where TABLE_NAME='MY_PRODUCTS';"
+                        cursor.execute(update_uk_products,id)
         conn.commit()
         cursor.close()
         conn.close()
@@ -27,6 +43,62 @@ def new_product(NAME,PRODUCT_TYPE_ID,AMOUNT,STATUS,GRADED,GRADING_COMPANY_ID,PRI
         print(e,": There was an error inserting a new product3")
     except Exception as e:
         print(e,": There was an error inserting a new product4")
+        traceback.print_exc()
     
 
 
+def update_product(NAME,PRODUCT_TYPE_ID,AMOUNT,STATUS,GRADED,PRICE,LAST_SOLD_PRICE):
+    try:
+        conn=get_connection()
+        cursor=conn.cursor()
+        cursor.execute("SELECT * FROM MY_PRODUCTS WHERE NAME='%s' AND product_type_id=%s;",(NAME,PRODUCT_TYPE_ID))
+        result=cursor.fetchone()
+        id=result["id"]
+        if cursor.rowcount >0:
+            query_up="""
+                    UPDATE TABLE MY PRODUCTS SET
+                    AMOUNT=%s,
+                    STATUS=%s,
+                    GRRADED=%s,
+                    PRICE=%s,
+                    LAST_SOLD_PRICE=%s
+                    where id=%s
+                    """
+            cursor.execute(query_up,(AMOUNT,STATUS,GRADED,PRICE,LAST_SOLD_PRICE,id))
+        cursor.close()
+        conn.commit()
+        conn.close()    
+        return 
+    except OperationalError as e:
+        print(e,": There was an error inserting a new product1")
+    except IntegrityError as e:
+        print(e,": There was an error inserting a new product2")
+    except ProgrammingError as e:
+        print(e,": There was an error inserting a new product3")
+    except Exception as e:
+        print(e,": There was an error inserting a new product4")
+        traceback.print_exc()
+
+
+def delete_product(ID):
+    try:
+        conn=get_connection()
+        cursor=conn.cursor()
+        query="""
+        DELETE FROM MY_PRODUCTS WHERE ID=%s;
+        """
+        cursor.execute(query,id)
+        if cursor.rowcount>0:
+            print("The product:",id," has been deleted")
+        else:
+            print("No rows deleted")
+        return
+    except OperationalError as e:
+        print(e,": There was an error inserting a new product1")
+    except IntegrityError as e:
+        print(e,": There was an error inserting a new product2")
+    except ProgrammingError as e:
+        print(e,": There was an error inserting a new product3")
+    except Exception as e:
+        print(e,": There was an error inserting a new product4")
+        traceback.print_exc()    
